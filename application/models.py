2fe63947e679ec59655d2029efbd1bd070d50b2a
abhyasgiri <abhyasgiri@outlook.com>
2020-12-16 13:23:28 +0000

    models column syntax error

--- application/models.py
+++ application/models.py
@@ -2,8 +2,8 @@ from application import db
 from datetime import datetime
 
 class Tasks(db.Model):
-    id = db.column(db.Integer, primary_key=True)
-    description = db.column(db.String(50), nullable=False)
-    completed = db.column(db.Boolean, nullable=False, default=False)
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(50), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
