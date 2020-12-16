ec301e418a2f10cd814dc8aba733c942cf21f125
abhyasgiri <abhyasgiri@outlook.com>
2020-12-16 10:11:36 +0000

    added a startup.sh

--- application/routes.py
+++ application/routes.py
@@ -1,24 +1,25 @@
 from application import app, db
 from application.models import Tasks
+from application.forms import TaskForm
+from flask import render_template, request, redirect, url_for
 
 @app.route("/")
 @app.route("/home")
 def home():
     all_tasks = Tasks.query.all()               #to view my tasks READ
     output = ""
-    if len(all_tasks) == 0                          # or can also write 'if not all_tasks:' 
-        return "No tasks have been created"
-    else: 
-        for task in all_tasks:
-            output +=   task.description + " - Completed?" + str(task.completed) "<br>"     #lists all tasks in homepage
-        return str(all_tasks)
+    return render_template("index.html", title="Home", all_tasks=all_tasks)
 
-@app.route("/create")                       #to add a new task CREATE
+@app.route("/create", methods = ["GET", "POST"])                       
 def create():
-    new_todo = Tasks(description = "New Task")
-    db.session.add(new_todo)
-    db.session.commit()
-    return "New task added" 
+    form = TaskForm()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            new_task = Tasks(description=form.description.data)
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect(url_for("home"))
+    return render_template ("add.html", title="Create a task", form=form)
 
 @app.route("/complete/<int:id>")            #to set a task to do as complete
 def complete(id):
@@ -34,17 +35,22 @@ def incomplete(id):
     db.session.commit()
     return f"Task {id} is incomplete"
 
-@app.route("/update/new_description>")              #to update something on the to do list
-def update(new_description):
-    task = Tasks.query.order_by(Tasks.id.desc()).first()
-    task.description = new_description
-    db.session.commit()
-    return f"Most recent taks was updated with the decription: {new_description}"
+@app.route("/update/<int:id>", methods = ["GET", "POST"])              #to update something on the to do list
+def update(id):
+    form = Form()
+    task = Tasks.query.filter_by(id=id).first()
+    if request.method == "POST":
+        task.description = form.description.data
+        db.session.commit()
+        return redirect(url_for("home"))
 
-@app.route("/delete/<int:id>")
+    return render_template("update.html", form=form, title="Update Task", task=task)
+
+
+@app.route("/delete/<int:id>", methods = ["GET", "POST"])
 def delete(id):
     task = Tasks.query.filter_by(id=id).first()
     db.session.delete(task)
     db.session.commit()
-    return f"Task {id} was deleted."
+    return redirect(url_for("home"))
 
